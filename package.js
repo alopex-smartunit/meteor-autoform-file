122f66931cad3abb21110a7cc90585bd747194ed
Андрей Лебедкин <duke@ymail.ru>
2017-05-11 07:36:47 +0900

    Remove hard dependencies

--- package.js
+++ package.js
@@ -1,21 +1,21 @@
 Package.describe({
   name: 'smartunit:autoform-files',
-  summary: 'File upload for AutoForm using ostrio:files (for AutoForm 6.0)',
-  description: 'File upload for AutoForm using ostrio:files (for AutoForm 6.0)',
-  version: '1.0.12',
+  summary: 'File upload for AutoForm using ostrio:files',
+  description: 'File upload for AutoForm using ostrio:files',
+  version: '1.0.13',
   git: 'https://github.com/alopex-smartunit/meteor-autoform-file.git'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@1.4');
+  api.versionsFrom('1.4');
 
   api.use([
     'check',
     'underscore',
     'reactive-var',
     'templating',
-    'aldeed:autoform@6.0.0',
-    'ostrio:files@1.7.11'
+    'aldeed:autoform',
+    'ostrio:files'
   ]);
 
   api.addFiles([
